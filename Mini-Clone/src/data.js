22e9b72361e531e6ec56f1b16b3690e732084e9f
SMITHAN KUMAR R <smithankumar404@gmail.com>
2022-10-31 23:22:55 +0530

    Airbnb clone

---
+++ Mini-Clone/src/data.js
@@ -0,0 +1,81 @@
+export default [
+    {
+        id: 1,
+        title: "Life Lessons with Katie Zaferes",
+        description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+        price: 136,
+        coverImg: "katie-zaferes.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "Online",
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        title: "Learn Wedding Photography",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 125,
+        coverImg: "wedding-photography.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Group Mountain Biking",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 550,
+        coverImg: "mountain-bike.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 6
+        },
+        location: "Norway",
+        openSpots: 3,
+    },
+    {
+        id: 4,
+        title: "Yoga",
+        description: "Bringing a mindful Yoga class into your event can be a special and memorable gift for your guests",
+        price: 250,
+        coverImg: "yoga.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 200
+        },
+        location: "online",
+        openSpots: 27,
+    },
+    {
+        id: 5,
+        title: "Get Creative: Virtual Arts & Crafts Classes",
+        description: "Create something new, brush up on your skills, or get tips for a DIY project with the help of these online arts and crafts events. Eventbrite’s experts have tracked down classes and workshops where you can paint, write, knit, and discover more amazing ways to express yourself from home.",
+        price: 500,
+        coverImg: "drawing.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 20
+        },
+        location: "online",
+        openSpots: 3,
+    },
+    {
+        id: 6,
+        title: "Cultural Holi Event",
+        description: "Holi also known as the Festival of Colours, the Festival of Spring, and the Festival of Love, is an ancient Hindu religious festival and one of the most",
+        price: 1500,
+        coverImg: "holi.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 200
+        },
+        location: "Washington",
+        openSpots: 10,
+    }
+
+]
